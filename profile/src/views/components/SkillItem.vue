19773c96ae08d094d42e07fa98d5e85178e428e4
Bryan Silverio <bryansilverio12@gmail.com>
2021-03-07 15:58:01 -0600

    potfolio

--- profile/src/views/components/SkillItem.vue
+++ profile/src/views/components/SkillItem.vue
@@ -1,13 +1,25 @@
 <template>
+  <div>
+    <ImageShadow
+      :alt="item.name"
+      :src="item.image"
+      width="120"
+      className="text-center img-fluid card"
+    />
+  </div>
+  <!--
   <div class="card">
     <div class="card-body text-center">
       <img :src="item.image" class="img-fluid mx-auto" />
     </div>
   </div>
+  -->
 </template>
 <script>
+import ImageShadow from "vue-image-shadow";
 export default {
   name: "SkillItem",
+  components: { ImageShadow },
   props: { item: Object },
   methods: {},
 };
