213dd913f721e7dae8d89941bf17346223c4d3b6
Bryan Silverio <bryansilverio12@gmail.com>
2021-09-05 00:29:44 -0500

    update skill section

--- profile/src/views/Skills.vue
+++ profile/src/views/Skills.vue
@@ -3,22 +3,10 @@
     <div class="max-width">
       <h2 class="title">Conocimientos</h2>
       <div class="skills-content">
-        <div class="column right">
-          <div class="bars" v-for="(item, index) in sideLeft" :key="index">
-            <div class="info">
-              <span>{{ item.name }}</span>
-              <span>90%</span>
-            </div>
-            <div class="line" :style="'width:' + item.value + '%'"></div>
-          </div>
-        </div>
-        <div class="column right">
-          <div class="bars" v-for="(item, index) in sideRigth" :key="index">
-            <div class="info">
-              <span>{{ item.name }}</span>
-              <span>90%</span>
-            </div>
-            <div class="line" :style="'width:' + item.value + '%'"></div>
+        <div class="card" v-for="(item, index) in data" :key="index">
+          <div class="box">
+            <i :class="item.icon"></i>
+            <div class="text">{{ item.name }}</div>
           </div>
         </div>
       </div>
@@ -32,24 +20,7 @@ export default {
     data: Array,
   },
   data() {
-    return {
-      sideLeft: [],
-      sideRigth: [],
-    };
-  },
-  created() {
-    this.getSkills(this.data);
-  },
-  methods: {
-    getSkills(items = []) {
-      items.forEach((element, index) => {
-        if (index % 2 == 0) {
-          this.sideLeft.push(element);
-        } else {
-          this.sideRigth.push(element);
-        }
-      });
-    },
+    return {};
   },
 };
 </script>
