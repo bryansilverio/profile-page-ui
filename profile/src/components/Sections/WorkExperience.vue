d06a687292fb1831ac41873d039268d64274fd7b
Bryan Silverio <bryansilverio12@gmail.com>
2022-11-01 16:49:53 -0600

    add workexperience component

--- profile/src/components/Sections/WorkExperience.vue
+++ profile/src/components/Sections/WorkExperience.vue
@@ -1,80 +1,60 @@
 <template>
- <section id="work" class="work_experience_area pt-115">
-<div class="container">
-<div class="row justify-content-center">
-<div class="col-lg-6">
-<div class="section_title text-center pb-30">
-<h5 class="sub_title">Experience</h5>
-<h3 class="main_title">Work Experience</h3>
-<ul class="line">
-<li></li>
-<li></li>
-<li></li>
-</ul>
-</div> 
-</div>
-</div> 
-<div class="work_experience">
-<div class="row">
-<div class="col-lg-12">
-<div class="single_experience d-lg-flex align-items-center flex-wrap wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.2s">
-<div class="experience_title order-last">
-<h3 class="title">Graphic Designer</h3>
-<p>PageBulb</p>
-<span class="date">March 2019- August 2024</span>
-</div>
-<div class="experience_number">
-<span>01</span>
-</div>
-<div class="experience_content order-first">
-<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr sedia magnaliquya ratsed diam Lorem ipsum dolor sit amconsetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam.Lorem ipsum dolor sit ametconsete tur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diamLorem ipsum dolor consetetur sadipscing elitr sed diam nonumy diam... </p>
-</div>
-</div> 
-</div>
-</div> 
-<div class="row">
-<div class="col-lg-12">
-<div class="single_experience d-lg-flex align-items-center flex-wrap wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.5s">
-<div class="experience_title text-lg-right">
-<h3 class="title">UI/UX Designer</h3>
-<p>UIdeck</p>
-<span class="date">March 2019- August 2019</span>
-</div>
-<div class="experience_number">
-<span>02</span>
-</div>
-<div class="experience_content">
-<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr sedia magnaliquya ratsed diam Lorem ipsum dolor sit amconsetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam.Lorem ipsum dolor sit ametconsete tur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diamLorem ipsum dolor consetetur sadipscing elitr sed diam nonumy diam... </p>
-</div>
-</div> 
-</div>
-</div> 
-<div class="row">
-<div class="col-lg-12">
-<div class="single_experience d-lg-flex align-items-center flex-wrap wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.8s">
-<div class="experience_title order-last">
-<h3 class="title">Web Developer</h3>
-<p>GrayGrids</p>
-<span class="date">June 2018- August 2019</span>
-</div>
-<div class="experience_number">
-<span>03</span>
-</div>
-<div class="experience_content order-first">
-<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr sedia magnaliquya ratsed diam Lorem ipsum dolor sit amconsetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam.Lorem ipsum dolor sit ametconsete tur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diamLorem ipsum dolor consetetur sadipscing elitr sed diam nonumy diam... </p>
-</div>
-</div> 
-</div>
-</div> 
-</div> 
-</div> 
-</section>
+    <section id="work" class="work_experience_area pt-115">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section_title text-center pb-30">
+                        <h5 class="sub_title">Experience</h5>
+                        <h3 class="main_title">Work Experience</h3>
+                        <ul class="line">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <WorkExperienceItem :workExperiences="workExperiences"/>
+            {{workExperiences.length}}
+
+        </div>
+    </section>
 </template>
 <script>
+import WorkExperienceItem from '@/components/_Shared/WorkExperienceItem.vue'
 export default {
     name: "WorkExperience",
+    components:{WorkExperienceItem},
     data() {
-        return {};
+        return {
+            workExperiences:[
+                {
+                    id:1,
+                    position:'Graphic Designer',
+                    company:'PageBulb',
+                    date:'March 2019- August 2024',
+                    number:'01',
+                    content:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr sedia magnaliquya ratsed diam Lorem ipsum dolor sit amconsetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam.Lorem ipsum dolor sit ametconsete tur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diamLorem ipsum dolor consetetur sadipscing elitr sed diam nonumy diam...'
+                },
+                {
+                    id:2,
+                    position:'Graphic Designer',
+                    company:'PageBulb',
+                    date:'March 2019- August 2024',
+                    number:'01',
+                    content:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr sedia magnaliquya ratsed diam Lorem ipsum dolor sit amconsetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam.Lorem ipsum dolor sit ametconsete tur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diamLorem ipsum dolor consetetur sadipscing elitr sed diam nonumy diam...'
+                },
+                {
+                    id:3,
+                    position:'Graphic Designer',
+                    company:'PageBulb',
+                    date:'March 2019- August 2024',
+                    number:'01',
+                    content:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr sedia magnaliquya ratsed diam Lorem ipsum dolor sit amconsetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam.Lorem ipsum dolor sit ametconsete tur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diamLorem ipsum dolor consetetur sadipscing elitr sed diam nonumy diam...'
+                }
+            ]
+        };
     },
 };
 </script>
---
+++ profile/src/components/_Shared/WorkExperienceItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="work_experience">
+        <div class="row" v-for="(v, i) in workExperiences" :key="i">
+            <div class="col-lg-12">
+                <div class="single_experience d-lg-flex align-items-center flex-wrap wow fadeIn"
+                    data-wow-duration="1.3s" data-wow-delay="0.2s"
+                    style="visibility: visible; animation-duration: 1.3s; animation-delay: 0.2s; animation-name: fadeIn;">
+                    <div :class="'experience_title ' + getPosition(i)">
+                        <h3 class="title">{{ v.position }}</h3>
+                        <p>{{ v.company }}</p>
+                        <span class="date">{{ v.date }}</span>
+                    </div>
+                    <div class="experience_number">
+                        <span>{{ v.number }}</span>
+                    </div>
+                    <div :class="'experience_content ' + getContent(i)">
+                        <p>{{ v.content }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "WorkExperienceItem",
+    props: {
+        workExperiences: Array
+    },
+    data() {
+        return {
+            positionItems: [
+                {
+                    title: 'text-lg-right',
+                    content: ''
+                },
+                {
+                    title: 'order-last',
+                    content: 'order-first'
+                }
+            ]
+
+        };
+    },
+    methods: {
+        getPosition(i = 0) {
+            let index = (i % 2) == 0 ? 1 : 0;
+            return this.positionItems[index].title
+        },
+        getContent(i = 0) {
+            let index = (i % 2) == 0 ? 1 : 0;
+            return this.positionItems[index].content
+        }
+    }
+};
+</script>
+  
