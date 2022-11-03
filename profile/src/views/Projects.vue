ef93f8d65c48d9cc7d0c5f6468d0eb4777bda84c
Bryan Silverio <bryansilverio12@gmail.com>
2022-11-03 11:55:42 -0600

    modify project page

--- profile/src/views/Projects.vue
+++ profile/src/views/Projects.vue
@@ -16,41 +16,17 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="project_btn text-sm-right">
-                        <a class="main-btn main-btn-2" href="#">All Works</a>
+                        <a class="main-btn main-btn-2" href="#">Todos</a>
                     </div>
                 </div>
             </div>
-            <div class="row project_active slick-initialized slick-slider" v-for="(v, i) in projects" :key="i">
-                
-                <div class="call_to_action_area">
-                    <div class="container">
-                        <div class="project_wrapper wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.5s">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="project_box d-md-flex justify-content-between align-items-center">
-                                        <div class="project_content">
-                                            <h3 class="project_title">{{ v.company.name }}</h3>
-                                            <ul class="line">
-                                                <li></li>
-                                                <li></li>
-                                                <li></li>
-                                            </ul>
-                                        </div> <!-- call to action content -->
 
-                                        <div class="project_logo">
-                                            <avatar :image="v.company.logo" :size="100" color="white"></avatar>
-                                        </div> <!-- call to action btn -->
-                                    </div> <!-- call to action box -->
-                                </div>
-                            </div>  <!-- row -->
-                        </div>  <!-- call to action wrapper -->
-                    </div> <!-- container -->
-                </div>
+            <div class="row project_active slick-initialized slick-slider">
 
                 <div class="slick-list">
                     <div class="slick-track">
                         <div class="row">
-                            <div class="col-lg-4" v-for="(p, i) in v.company.projects" :key="i">
+                            <div class="col-lg-4" v-for="(p, i) in projects" :key="i">
                                 <div class="single_project wow fadeInUpBig">
                                     <div class="project_image">
                                         <img :src="p.image" alt="project">
@@ -59,10 +35,27 @@
                                         <h4 class="project_title">{{ p.name }}</h4>
                                         <p class="project_content">{{ p.description }}</p>
                                         <p class="project_content">{{ p.type }}</p>
-                                        <template v-if="p.technologies.length">
-                                            <vue-badges v-for="(t, i) in p.technologies" :key="i" :badges='[[t.name]]'>
-                                            </vue-badges>
-                                        </template>
+                                        <span class="project_content">
+                                            <table>
+                                                <tr>
+                                                    <td>
+                                                        <avatar :image="p.company.logo" :size="50" color="white">
+                                                        </avatar>
+                                                    </td>
+                                                    <td>
+                                                        <span style="margin-right: 20px">{{ p.company.name }}</span>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </span>
+                                        <span class="project_content">
+                                            <template v-if="p.technologies.length">
+                                                <vue-badges v-for="(t, i) in p.technologies" :key="i"
+                                                    :badges='[[t.name]]'>
+                                                </vue-badges>
+                                            </template>
+                                        </span>
+
                                     </div>
                                 </div>
                             </div>
@@ -77,12 +70,16 @@
 <script>
 import Avatar from 'vue-avatar-component'
 import projects_data from '@/data/projects.json'
+import workExperiences_data from '@/data/work-experiences.json'
 export default {
     name: "Projects",
     components: { Avatar },
     computed: {
         projects() {
             return projects_data
+        },
+        workExperiences() {
+            return workExperiences_data
         }
     }
 };
