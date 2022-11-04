2105c77a5823b0a34e97501d425e29294a3d16ea
Bryan Silverio <bryansilverio12@gmail.com>
2022-11-03 18:07:23 -0600

    add 404 page

--- profile/src/router/index.js
+++ profile/src/router/index.js
@@ -61,7 +61,22 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () =>
           import(/* webpackChunkName: "about" */ "@/views/Projects.vue"),
-      }
+      },
+      {
+        path: "/projects/:id",
+        name: "project-details",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "about" */ "@/views/ProjectDetails.vue"),
+      },
+      {
+        path: "/*",
+        name: "not-found",
+        component: () =>
+          import(/* webpackChunkName: "about" */ "@/views/errors/404.vue"),
+      },
     ],
   },
 ];
---
+++ profile/src/views/errors/404.vue
@@ -0,0 +1,25 @@
+<template>
+    <section id="services" class="services_area pt-115 pb-120">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section_title text-center pb-30">
+                        <h5 class="sub_title"></h5>
+                        <h3 class="main_title">Página no encontrada</h3>
+                        <ul class="line">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+</template>
+<script>
+export default {
+    name: "NotFound",
+};
+</script>
+  
