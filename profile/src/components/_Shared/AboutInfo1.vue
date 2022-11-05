0f621bd36348bfb2e1c192c05cefc2dc0fcc4a42
Bryan Silverio <bryansilverio12@gmail.com>
2022-11-05 01:55:53 -0600

    working about page

---
+++ profile/src/components/_Shared/AboutInfo1.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="info-one">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12 col-12">
+
+                    <div class="info-text wow fadeInLeft" data-wow-delay=".3s"
+                        style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+                        <div class="main-icon">
+                            <i class="lni lni-inbox"></i>
+                        </div>
+                        <h2>{{ title }}</h2>
+                        <div v-html="description"></div>
+                        <div class="button">
+                            <a href="javascript:void(0)" class="btn">Get Started</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-6 col-md-12 col-12">
+                    <div class="info-image wow fadeInRight" data-wow-delay=".5s"
+                        style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInRight;">
+                        <img class="ss1" :src="image" alt="#" style="border-radius: 50%">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AboutInfo1',
+    props: {
+        title: {
+            type: String,
+            default: ''
+        },
+        description: {
+            type: String,
+            default: ''
+        },
+        image: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
--- profile/src/components/_Shared/Services.vue
+++ profile/src/components/_Shared/Services.vue
@@ -3,6 +3,7 @@
 
     <section id="services" class="services_area pt-115 pb-120">
         <div class="container">
+
             <div class="row justify-content-center">
                 <div class="col-lg-6">
                     <div class="section_title text-center pb-30">
@@ -13,9 +14,29 @@
                             <li></li>
                             <li></li>
                         </ul>
-                    </div> <!-- section title -->
+                    </div>
                 </div>
-            </div> <!-- row -->
+            </div>
+
+            <div v-for="(v, i) in activities" :key="i">
+                <AboutInfo1 v-if="i % 2 == 0" :title="v.title" :description="v.description" :image="v.image" />
+                <AboutInfo2 v-else :title="v.title" :description="v.description" :image="v.image" />
+            </div>
+
+            <!--
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section_title text-center pb-30">
+                        <h5 class="sub_title">Actividades</h5>
+                        <h3 class="main_title">Principales actividades</h3>
+                        <ul class="line">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>  
+                </div>
+            </div>  
             <div class="row">
                 <div class="col-lg-4 col-sm-6" v-for="(v, i) in activities" :key="i">
                     <div class="single_service mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.2s">
@@ -26,18 +47,21 @@
                             <h4 class="service_title"><a href="#">{{ v.title }}</a></h4>
                             <div v-html="v.description"></div>
                         </div>
-                    </div> <!-- single service -->
+                    </div>  
                 </div>
-            </div> <!-- row -->
-        </div> <!-- container -->
+            </div> -->
+        </div>
     </section>
 
     <!--====== SERVICES PART ENDS ======-->
 </template>
 <script>
 import activities_data from '@/data/activities.json'
+import AboutInfo1 from '@/components/_Shared/AboutInfo1.vue'
+import AboutInfo2 from '@/components/_Shared/AboutInfo2.vue'
 export default {
     name: "Services",
+    components: { AboutInfo1, AboutInfo2 },
     computed: {
         activities() {
             return activities_data
