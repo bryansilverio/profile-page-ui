ca029fd99dc1fb068d628c20cb2956b72da5668f
Bryan Silverio <bryansilverio12@gmail.com>
2022-11-06 02:01:25 -0600

    working in 404 page

--- profile/src/views/errors/404.vue
+++ profile/src/views/errors/404.vue
@@ -2,10 +2,10 @@
     <section id="services" class="services_area pt-115 pb-120">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <div class="section_title text-center pb-30">
-                        <h5 class="sub_title"></h5>
-                        <h3 class="main_title">Página no encontrada</h3>
+                        <h3 class="main_title">404</h3>
+                        <h5 class="sub_title">Página no encontrada</h5>
                         <ul class="line">
                             <li></li>
                             <li></li>
@@ -14,6 +14,20 @@
                     </div> <!-- section title -->
                 </div>
             </div> <!-- row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center">
+                        <span>La página que buscabas no existe.</span>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="text-center">
+                        <router-link class="main-btn-home-about mt-20" to="/">
+                            Regresar al inicio <i class="lni lni-chevron-left-circle"></i>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
         </div> <!-- container -->
     </section>
 </template>
