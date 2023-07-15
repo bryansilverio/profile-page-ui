27731f540958e75c566567d66fcd820362f68cf9
Bryan Silverio <bryansilverio12@gmail.com>
2023-07-14 19:51:31 -0600

    modify notfound page

--- profile/src/views/errors/404.vue
+++ profile/src/views/errors/404.vue
@@ -1,31 +1,24 @@
 <template>
-    <section id="services" class="services_area pt-115 pb-120">
+    <section id="services" class="services_area pt-115 pb-120" style="margin-bottom: 200px;">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-12">
+            <div class="row justify-content-center" style="margin-top: 100px;">
+                <div class="col-lg-6">
+                    <div class="text-start" style="margin-top: 200px;">
+                        <span style="font-size: large;">La página que buscas no fue encontrada.</span>
+                    </div>
+                    <router-link class="main-btn-home-about mt-20" to="/">
+                        Regresar al inicio <i class="lni lni-chevron-left-circle"></i>
+                    </router-link>
+                </div>
+                <div class="col-lg-6">
                     <div class="section_title text-center pb-30">
-                        <h3 class="main_title">404</h3>
+                        <h3 class="main_title" style="margin-top: 10;">
+                            <span style="font-size: 15rem; color: #FF474C;">4</span>
+                            <span style="font-size: 15rem;">0</span>
+                            <span style="font-size: 15rem; color:#FF474C">4</span>
+                        </h3>
                         <h5 class="sub_title">Página no encontrada</h5>
-                        <ul class="line">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
                     </div> <!-- section title -->
-                </div>
-            </div> <!-- row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="text-center">
-                        <span>La página que buscabas no existe.</span>
-                    </div>
-                </div>
-                <div class="col-lg-12">
-                    <div class="text-center">
-                        <router-link class="main-btn-home-about mt-20" to="/">
-                            Regresar al inicio <i class="lni lni-chevron-left-circle"></i>
-                        </router-link>
-                    </div>
                 </div>
             </div>
         </div> <!-- container -->
