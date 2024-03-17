24d98a8827ed3c357cc2f729ac9ccee634680fb1
Bryan Silverio <bryansilverio12@gmail.com>
2024-03-17 13:44:46 -0600

    update template

--- profile/src/sections/NavDesktop.vue
+++ profile/src/sections/NavDesktop.vue
@@ -3,27 +3,24 @@
 		<div class="text-center">
 			<span style="color: #FF474C;">B</span><span style="color: white;">SILVERIO</span>
 			<div class="author-img" style="background-image: url(assets/images/profile-pic.png);"></div>
-			<h1 id="colorlib-logo" ><a href="/" style="color: #FF474C;">{{$t('name')}}</a></h1>
-			<span class="position"><a href="#">{{$t('position')}}</a></span>
+			<h1 id="colorlib-logo"><a href="/" style="color: #FF474C;font-weight: bolder;">{{$t('name')}}</a></h1>
+			<span class="position"><a href="">{{$t('position')}}</a></span>
 		</div>
 		<nav id="colorlib-main-menu" role="navigation" class="navbar">
 			<div id="navbar" class="collapse">
 				<ul>
-					<li><a href="#" data-nav-section="home" style="color: white;">{{ $t('menu.home') }}</a></li>
+					<li><a href="#home" data-nav-section="home" style="color: white;">{{ $t('menu.home') }}</a></li>
 					<li><a href="#about" data-nav-section="about" style="color: white;">{{ $t('menu.aboutMe') }}</a></li>
-					<li><a href="#" data-nav-section="services" style="color: white;">Services</a></li>
 					<li><a href="#skills" data-nav-section="skills" style="color: white;">{{ $t('menu.skills') }}</a></li>
-					<li><a href="#" data-nav-section="education" style="color: white;">Education</a></li>
 					<li><a href="#experience" data-nav-section="experience" style="color: white;">{{ $t('menu.experience') }}</a></li>
 					<li><a href="#projects" data-nav-section="work" style="color: white;">{{ $t('menu.projects') }}</a></li>
-					<li><a href="#" data-nav-section="blog" style="color: white;">Blog</a></li>
 					<li><a href="#contact" data-nav-section="contact" style="color: white;">{{ $t('menu.contact') }}</a></li>
 				</ul>
 			</div>
 		</nav>
 
 		<div class="colorlib-footer">
-			<p>Copyright &#169;
+			<p style="color: #FF474C;">Copyright &#169;
 				{{ new Date().getFullYear() }}
 				<a href="https://bryansilverio.com/" class="hover:underline"> Bryan Silverio</a>.
 				All Rights Reserved.
