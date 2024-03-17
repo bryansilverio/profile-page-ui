219ba8a3a939f5fd7fc5533c14c94f3697b55cd1
Bryan Silverio <bryansilverio12@gmail.com>
2024-03-17 14:24:01 -0600

    update template

--- profile/src/sections/NavDesktop.vue
+++ profile/src/sections/NavDesktop.vue
@@ -9,7 +9,7 @@
 		<nav id="colorlib-main-menu" role="navigation" class="navbar">
 			<div id="navbar" class="collapse">
 				<ul>
-					<li><a href="#home" data-nav-section="home" style="color: white;">{{ $t('menu.home') }}</a></li>
+					<li><a href="#colorlib-hero" data-nav-section="home" style="color: white;">{{ $t('menu.home') }}</a></li>
 					<li><a href="#about" data-nav-section="about" style="color: white;">{{ $t('menu.aboutMe') }}</a></li>
 					<li><a href="#skills" data-nav-section="skills" style="color: white;">{{ $t('menu.skills') }}</a></li>
 					<li><a href="#experience" data-nav-section="experience" style="color: white;">{{ $t('menu.experience') }}</a></li>
--- profile/src/sections/Skills.vue
+++ profile/src/sections/Skills.vue
@@ -4,19 +4,23 @@
             <div class="row">
                 <div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                     <span class="heading-meta">{{ $t('sections.skills') }}</span>
-                    <h2 class="colorlib-heading animate-box">My Skills</h2>
+                    <h2 class="colorlib-heading animate-box">{{ $t('sections.skills') }}</h2>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
                     <p>Las herramientas y tecnologías que he usado en mis proyectos y en los que tengo experiencia son los siguientes:</p>
                 </div>
-                <div class="col-md-2 animate-box" data-animate-effect="fadeInLeft" v-for="(v, i) in skills" :key="i">
-                    <article>
-                        <img :src="v.icon" alt="Experience icon" class="icon" style="width: 30px;"/>
-                    </article>
-                </div>
             </div>
+            <div class="row row-pt-md">
+				<div class="col-md-2 text-center animate-box" v-for="(v, i) in skills" :key="i">
+					<div class="services" style="border-bottom:0px">
+						<span class="icon">
+							<img :src="v.icon" alt="Experience icon" class="icon" style="width: 45px; margin: 25px;"/>
+						</span>
+					</div>
+				</div>
+			</div>
         </div>
     </section>
 </template>
@@ -37,3 +41,14 @@ export default defineComponent({
   }
 });
 </script>
+<style>
+.services .icon::before{
+    border-color: #222233;
+}
+.services .icon::after{
+    border-color: #222233;
+}
+.services .icon{
+    background: #222233;
+}
+</style>
